8745128d587af613a72d725e299d80ed4dda43a4
jbwg312 <jbwg312@gmail.com>
2016-09-06 18:04:19 -0600

    initial commit

---
+++ client/scripts/controllers/chatsController.js
@@ -0,0 +1,18 @@
+import Moment from 'moment';
+import { Controller } from 'angular-ecmascript/module-helpers';
+import { Chats } from '../../../lib/collections';
+
+export default class ChatsCtrl extends Controller {
+	constructor(){
+		super(...arguments);
+
+		this.helpers({
+			data(){
+				return Chats.find();
+			}
+		});
+}
+	remove(chat) {
+		this.data.splice(this.data.indexOf(chat), 1);
+	}
+}
